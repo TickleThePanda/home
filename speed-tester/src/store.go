a2c6aacf7b8d51e752090e4d793347a373788c78
Panda (Thomas) Attwood | TickleThePanda <thomas.pg.attwood@gmail.com>
2021-02-21 10:56:09 +0000

    Sort entries descending

--- speed-tester/src/store.go
+++ speed-tester/src/store.go
@@ -2,62 +2,11 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
-	"strconv"
+	"sort"
 	"strings"
 	"time"
 )
-
-type SpeedTestResult struct {
-	Time          time.Time
-	ServerId      string
-	ServerName    string
-	Distance      float64
-	Latency       time.Duration
-	DownloadSpeed float64
-	UploadSpeed   float64
-}
-
-func (r *SpeedTestResult) String() string {
-	return fmt.Sprintf(
-		"%s, %s, %s, %f, %s, %f, %f",
-		r.Time,
-		r.ServerId, r.ServerName,
-		r.Distance, r.Latency,
-		r.DownloadSpeed, r.UploadSpeed,
-	)
-}
-
-func (r *SpeedTestResult) ToCsv() string {
-	return fmt.Sprintf(
-		"%s,%s,%s,%f,%s,%f,%f",
-		r.Time.Format(time.RFC3339),
-		r.ServerId, r.ServerName,
-		r.Distance, r.Latency.String(),
-		r.DownloadSpeed, r.UploadSpeed,
-	)
-}
-
-func SpeedTestResultFromCsv(csv string) *SpeedTestResult {
-	sp := strings.Split(csv, ",")
-
-	t, _ := time.Parse(time.RFC3339, sp[0])
-	dist, _ := strconv.ParseFloat(sp[3], 10)
-	latency, _ := time.ParseDuration(sp[4])
-	down, _ := strconv.ParseFloat(sp[5], 10)
-	up, _ := strconv.ParseFloat(sp[6], 10)
-
-	return &SpeedTestResult{
-		Time:          t,
-		ServerId:      sp[1],
-		ServerName:    sp[2],
-		Distance:      dist,
-		Latency:       latency,
-		DownloadSpeed: down,
-		UploadSpeed:   up,
-	}
-}
 
 type SpeedTestResults struct {
 	Entries []*SpeedTestResult
@@ -65,7 +14,9 @@ type SpeedTestResults struct {
 }
 
 func (rs *SpeedTestResults) RecentEntries() []*SpeedTestResult {
-	return FilterResultsWithinLast(rs.Entries, time.Duration(24)*time.Hour)
+	recentEntries := FilterResultsWithinLast(rs.Entries, time.Duration(24)*time.Hour)
+	sort.Sort(sort.Reverse(ByDate(recentEntries)))
+	return recentEntries
 }
 
 func (rs *SpeedTestResults) String() string {
