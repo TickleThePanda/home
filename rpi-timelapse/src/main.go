1b39d4ba3fa944a3e6538454c0739e7ad2e4331d
Panda (Thomas) Attwood | TickleThePanda <thomas.pg.attwood@gmail.com>
2021-09-05 00:31:20 +0100

    Add timelapse list

--- rpi-timelapse/src/main.go
+++ rpi-timelapse/src/main.go
@@ -1,50 +1,50 @@
 package main
 
 import (
-	"log"
-	"os"
+  "log"
+  "os"
 )
 
 func main() {
 
-	siteRoot := os.Getenv("RPI_CAMERA_SITE_ROOT")
-	if siteRoot == "" {
-		siteRoot = ""
-	}
-	sharedAssets := os.Getenv("RPI_CAMERA_SHARED_ASSETS_SITE")
-	if siteRoot == "" {
-		sharedAssets = ""
-	}
+  siteRoot := os.Getenv("RPI_CAMERA_SITE_ROOT")
+  if siteRoot == "" {
+    siteRoot = ""
+  }
+  sharedAssets := os.Getenv("RPI_CAMERA_SHARED_ASSETS_SITE")
+  if siteRoot == "" {
+    sharedAssets = ""
+  }
 
-	storageDirectory := os.Getenv("RPI_CAMERA_STORAGE_DIR")
-	if storageDirectory == "" {
-		storageDirectory = "./data"
-	}
+  storageDirectory := os.Getenv("RPI_CAMERA_STORAGE_DIR")
+  if storageDirectory == "" {
+    storageDirectory = "./data"
+  }
 
-	store := &TimelapseStore{
-		StorageDirectory: storageDirectory,
-	}
+  store := &TimelapseStore{
+    StorageDirectory: storageDirectory,
+  }
 
-	timelapseCamera := &TimelapseCamera{
-		Store: store,
-	}
+  timelapseCamera := &TimelapseCamera{
+    Store: store,
+  }
 
-	store.Init()
+  store.Init()
 
-	timelapse, err := store.GetCurrentTimelapse()
-	if err != nil {
-		log.Printf("Unable to get current timelapse: %s", err.Error())
-	} else {
-		log.Printf("Starting existing timelapse %+v", timelapse)
-		go timelapseCamera.StartTimelapse(timelapse)
-	}
+  timelapse, err := store.GetCurrentTimelapse()
+  if err != nil {
+    log.Printf("Unable to get current timelapse: %s", err.Error())
+  } else {
+    log.Printf("Starting existing timelapse %+v", timelapse)
+    go timelapseCamera.StartTimelapse(timelapse)
+  }
 
-	handleRequests(
-		&SiteInfo{
-			SiteRoot:         siteRoot,
-			SharedAssetsSite: sharedAssets,
-		},
-		store,
-		timelapseCamera,
-	)
+  handleRequests(
+    &SiteInfo{
+      SiteRoot:         siteRoot,
+      SharedAssetsSite: sharedAssets,
+    },
+    store,
+    timelapseCamera,
+  )
 }
--- rpi-timelapse/src/timelapse-camera.go
+++ rpi-timelapse/src/timelapse-camera.go
@@ -1,121 +1,121 @@
 package main
 
 import (
-	"bytes"
-	"errors"
-	"fmt"
-	"log"
-	"strings"
-	"sync"
-	"time"
+  "bytes"
+  "errors"
+  "fmt"
+  "log"
+  "strings"
+  "sync"
+  "time"
 
-	"github.com/dhowden/raspicam"
+  "github.com/dhowden/raspicam"
 )
 
 type TimelapseCamera struct {
-	Store            *TimelapseStore
-	CurrentTicker    *time.Ticker
-	Mutex            sync.Mutex
-	RequestsChannel  chan<- *CameraSettings
-	ResponsesChannel <-chan ImageResult
+  Store            *TimelapseStore
+  CurrentTicker    *time.Ticker
+  Mutex            sync.Mutex
+  RequestsChannel  chan<- *CameraSettings
+  ResponsesChannel <-chan ImageResult
 }
 
 func (tt *TimelapseCamera) StartTimelapse(t *TimelapseSettings) {
 
-	reqs, res := tt.CameraServer()
-	tt.RequestsChannel = reqs
-	tt.ResponsesChannel = res
+  reqs, res := tt.CameraServer()
+  tt.RequestsChannel = reqs
+  tt.ResponsesChannel = res
 
-	if tt.CurrentTicker != nil {
-		tt.CurrentTicker.Stop()
-		tt.CurrentTicker = nil
-	}
+  if tt.CurrentTicker != nil {
+    tt.CurrentTicker.Stop()
+    tt.CurrentTicker = nil
+  }
 
-	log.Printf("Starting timelapse ticker with interval %v", t.Interval)
-	tt.CurrentTicker = time.NewTicker(t.Interval)
-	go func() {
-		for range tt.CurrentTicker.C {
-			log.Printf("Taking image at triggered interval")
+  log.Printf("Starting timelapse ticker with interval %v", t.Interval)
+  tt.CurrentTicker = time.NewTicker(t.Interval)
+  go func() {
+    for range tt.CurrentTicker.C {
+      log.Printf("Taking image at triggered interval")
 
-			err := tt.Store.StoreImage(tt.CaptureImage)
-			if err != nil {
-				log.Printf("Error storing image: %s", err.Error())
-			}
-		}
-	}()
+      err := tt.Store.StoreImage(tt.CaptureImage)
+      if err != nil {
+        log.Printf("Error storing image: %s", err.Error())
+      }
+    }
+  }()
 
-	err := tt.Store.StoreImage(tt.CaptureImage)
-	if err != nil {
-		log.Printf("Error storing image: %s", err.Error())
-	}
+  err := tt.Store.StoreImage(tt.CaptureImage)
+  if err != nil {
+    log.Printf("Error storing image: %s", err.Error())
+  }
 
 }
 
 type ImageResult struct {
-	Bytes []byte
-	Error error
+  Bytes []byte
+  Error error
 }
 
 func (c *TimelapseCamera) CameraServer() (requests chan<- *CameraSettings, responses <-chan ImageResult) {
-	reqs := make(chan *CameraSettings, 1)
-	ress := make(chan ImageResult, 1)
+  reqs := make(chan *CameraSettings, 1)
+  ress := make(chan ImageResult, 1)
 
-	go func() {
-		for cameraSettings := range reqs {
+  go func() {
+    for cameraSettings := range reqs {
 
-			s := raspicam.NewStill()
-			s.Camera.VFlip = cameraSettings.VFlip
-			s.Camera.HFlip = cameraSettings.HFlip
-			s.Camera.Rotation = cameraSettings.Rotation
-			s.Camera.MeteringMode = raspicam.MeteringAverage
-			s.Camera.AWBMode = raspicam.AWBOff
-			s.Camera.ISO = 200
-			s.Args = []string{"--flicker", "50hz", "-awbg", "1.7,1.9", "--drc", "high"}
-			s.Width = cameraSettings.Width
-			s.Height = cameraSettings.Height
-			s.Encoding = raspicam.EncodingPNG
+      s := raspicam.NewStill()
+      s.Camera.VFlip = cameraSettings.VFlip
+      s.Camera.HFlip = cameraSettings.HFlip
+      s.Camera.Rotation = cameraSettings.Rotation
+      s.Camera.MeteringMode = raspicam.MeteringAverage
+      s.Camera.AWBMode = raspicam.AWBOff
+      s.Camera.ISO = 200
+      s.Args = []string{"--flicker", "50hz", "-awbg", "1.7,1.9", "--drc", "high"}
+      s.Width = cameraSettings.Width
+      s.Height = cameraSettings.Height
+      s.Encoding = raspicam.EncodingPNG
 
-			errCh := make(chan error)
+      errCh := make(chan error)
 
-			wasError := false
-			var imageResultError strings.Builder
-			go func() {
-				for x := range errCh {
-					wasError = true
-					imageResultError.WriteString(x.Error())
-					imageResultError.WriteRune('\n')
-				}
-			}()
+      wasError := false
+      var imageResultError strings.Builder
+      go func() {
+        for x := range errCh {
+          wasError = true
+          imageResultError.WriteString(x.Error())
+          imageResultError.WriteRune('\n')
+        }
+      }()
 
-			var b *bytes.Buffer = &bytes.Buffer{}
-			log.Println("Capturing image")
-			raspicam.Capture(s, b, errCh)
-			log.Println("Returning image")
+      var b *bytes.Buffer = &bytes.Buffer{}
+      log.Println("Capturing image")
+      raspicam.Capture(s, b, errCh)
+      log.Println("Returning image")
 
-			if wasError {
-				ress <- ImageResult{
-					Error: errors.New(imageResultError.String()),
-				}
-			} else {
-				ress <- ImageResult{
-					Bytes: b.Bytes(),
-				}
-			}
-		}
-	}()
+      if wasError {
+        ress <- ImageResult{
+          Error: errors.New(imageResultError.String()),
+        }
+      } else {
+        ress <- ImageResult{
+          Bytes: b.Bytes(),
+        }
+      }
+    }
+  }()
 
-	return reqs, ress
+  return reqs, ress
 }
 
 func (c *TimelapseCamera) CaptureImage(cameraSettings *CameraSettings) ([]byte, error) {
 
-	log.Println("Requesting image")
-	c.RequestsChannel <- cameraSettings
-	result := <-c.ResponsesChannel
+  log.Println("Requesting image")
+  c.RequestsChannel <- cameraSettings
+  result := <-c.ResponsesChannel
 
-	if result.Error != nil {
-		return nil, fmt.Errorf("error getting image: %w", result.Error)
-	}
+  if result.Error != nil {
+    return nil, fmt.Errorf("error getting image: %w", result.Error)
+  }
 
-	return result.Bytes, nil
+  return result.Bytes, nil
 }
